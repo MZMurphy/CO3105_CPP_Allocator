0bce67ed312a02f42086daead8cd3f9febfb1cb4
mm1053@student.le.ac.uk <129211156+MZMurphy@users.noreply.github.com>
2025-12-14 13:04:55 +0000

    Add DEBUG macro. Used for debuging. ON == 1 in .h, OFF == 0 or undefined in .h

---
+++ DEBUG.h
@@ -0,0 +1,13 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#include <iostream>
+#define DEBUG 1
+
+#if DEBUG
+    #define DEBUG_PRINT(x) std::cout << x
+#else
+    #define DEBUG_PRINT(x)
+
+#endif //DEBUG
+#endif //DEBUG_H

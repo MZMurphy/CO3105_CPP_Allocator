7e315b48567cd5b7dcc8b5f7c276aa520814ecbf
Simran Sajan <111128218+simransarah@users.noreply.github.com>
2025-12-15 16:08:12 +0000

    Rename allocation method from run_allocation to perform_allocation (leftover from before I started)

--- main.cpp
+++ main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char* argv[])
     allocator.load_projects(projectFile);
     allocator.load_students(studentFile);
 
-    allocater.run_allocation();
+    allocater.perform_allocation();
     allocator.save_allocation(outputFile);
 
     return 0; // Success.
